825b50fbae831b33c5c6e221e06cf3774d3f2baa
Armando Lezama <armando.lezama.psic@gmail.com>
2019-06-04 21:27:19 -0500

    Se creó una función que escribe un archivo y posteriormente lo lee, añadiendo el texto en la página, todo sobre funciones fs.readFile y fs.write

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const http = require('http');
+const fs = require('fs');
 const port = 8000;
 
-function responseHandler(request, response){
+const textoDePrueba = 'Js y node son herramientas  fáciles que deben de usarse correctamente para implementar una web, la parte del server es confusa pero se puede lograr aprender.'
 
-    response.write("Hola desde el servidor");
-    response.end();
+function responseHandler(request, response){
+    fs.writeFile('historia.txt', textoDePrueba, function(err){
+        if(err){
+            throw err;
+        }
+    })
+
+    fs.readFile('historia.txt', {encoding: "utf8"}, function(err, contenido){
+        if (err){
+            throw err;
+        }
+
+        response.write(contenido);
+        response.end();
+    })
 }
 
+
+
 const server = http.createServer(responseHandler);
+
+
 
 function callback(){
     console.log("El servidor está funcionando en el puerto " + port)
